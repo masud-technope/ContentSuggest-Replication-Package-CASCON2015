d71c40fb568b635cb2a3be01ad146e0a05d36428
MasudRahman <masudcseku@gmail.com>
2019-05-21 22:27:06 -0600

    Base version of CASCON 2015

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# ContentSuggest-Replication-Package-CASCON2015
-Recommending Relevant Sections from a Webpage About Programming Errors and Exceptions
+# Recommending Relevant Sections from a Web page about Programming Errors and Exceptions
+
+Accepted Answer at CASCON 2015
+-----------------------------
+```
+Recommending Relevant Sections from a Webpage about Programming Errors and Exceptions
+Mohammad Masudur Rahman and Chanchal K. Roy
+```
+
+**Abstract:**  Programming errors or exceptions are inherent in software development and maintenance, and given today's Internet era, software developers often look at web for finding working solutions. They make use of a search engine for retrieving relevant pages, and then look for the appropriate solutions by manually going through the pages one by one. However, both the manual checking of a page content against a given exception (and its context) and then working an appropriate solution out are non-trivial tasks. They are even more complex and time-consuming with the bulk of irrelevant (i.e., off-topic) and noisy (e.g., advertisements) content in the web page. In this paper, we propose an IDE-based and context-aware page content recommendation approach that locates and recommends relevant sections of a web page by exploiting the technical details, in particular the context, of an encountered exception in the IDE. A preliminary evaluation with 250 web pages related to 80 programming errors and exceptions and comparison against one existing approach show that the proposed approach is highly promising in terms of precision, recall and F1-measure.
+
+Experiment Data
+-----------------------------
+**Exception Details**
+ - Stack traces (150)
+ - Contextual code segments (60)
+
+**Relevant Content Corpus**
+- HTML web pages (250)
+- Ground truth (HTML) (250)
+- Ground truth (TXT) (250)
+
+Working Prototype/Plug-in
+--------------------
+- Eclipse plug-in
+- Plug-in dependencies
+
+Please cite our work as
+------------
+```
+@inproceedings{cascon2015masud, 
+author = {Rahman, Mohammad Masudur and Roy, Chanchal K.}, 
+title = {Recommending Relevant Sections from a Webpage About Programming Errors and Exceptions}, 
+booktitle = {Proc. CASCON}, 
+year = {2015}, 
+pages = {181--190} }
+
+```
+## Something not working as expected?
+
+Contact:  **Masud Rahman**  ([masud.rahman@usask.ca](mailto:masud.rahman@usask.ca))
+
+OR
+
+Create an issue from  [here](https://github.com/masud-technope/ContentSuggest-Replication-Package-CASCON2015/issues/new)
---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>ContentSuggest</title>
+<link href="../css/style1.css" type="text/css" rel="stylesheet" />
+<style type="text/css">
+hl
+{
+ color:#0066FF;
+ font-size:18px;
+}
+.important
+{
+ color:#FF0000;
+ font-weight:bold;
+}
+
+
+</style>
+</head>
+
+<body>
+<table border="0" cellspacing="0" width="1000" align="center">
+<tr>
+<td width="70">
+<img src="img/suggestion.png" width="50" height="50" />
+</td>
+<td>
+<h1>ContentSuggest</h1>
+</td>
+</tr>
+<tr>
+<td colspan="2">
+<div class="rounded2">
+<strong>Abstract:</strong> Programming errors or exceptions are inherent in software development and maintenance, and given today's Internet era, software developers often look at web for finding working solutions. They make use of a search engine for retrieving relevant pages, and then look for the appropriate solutions by manually going through the pages one by one. However, both the manual checking of a page content against a given exception (and its context) and then working an appropriate solution out are non-trivial tasks. They are even more complex and time-consuming with the bulk of irrelevant (i.e., off-topic) and noisy (e.g., advertisements) content in the web page. In this paper, we propose an IDE-based and context-aware page content recommendation approach that locates and recommends relevant sections of a web page by exploiting the technical details, in particular the context, of an encountered exception in the IDE. A preliminary evaluation with 250 web pages related to 80 programming errors and exceptions and comparison against one existing approach show that the proposed approach is highly promising in terms of precision, recall and F1-measure.
+</div>
+</td>
+</tr>
+<tr>
+<td colspan="2">
+<div>
+<p style="font-weight:bold">Experiment Data</p>
+<hr />
+<p style="font-weight:bold">Exception Details</p>
+<ul>
+<li><a href="https://drive.google.com/open?id=1nqECpTBiu5lu9nJWGRAKYZqn4u9pnPry">Stack Traces (150)</a></li>
+<li><a href="https://drive.google.com/open?id=1wBsFam8sHUhQd7gO4SuAy3pijYbQuT8w">Context Code Segments (60)</a></li>
+</ul>
+<p style="font-weight:bold">Relevant Content Data</p>
+<ul>
+<li><a href="https://drive.google.com/open?id=1HDdggaHa1ezururI50aaxELHuMHcIhlg">250 HTML Pages</a></li>
+<li><a href="https://drive.google.com/open?id=1ACY-qt6J8wSOMc1I_ZTolEJTN4CtgkLv">Relevant Gold Content (HTML)</a></li>
+<li><a href="https://drive.google.com/open?id=1M3PHGn5AihX7PggP_YnHak3PzmaoUsOx">Relevant Gold Content (TXT)</a></li>
+</ul>
+</div>
+</td>
+</tr>
+<tr>
+<td colspan="2">
+<p style="font-weight:bold">Other Resources</p>
+<hr />
+<div>
+<ul>
+<li><a href="https://drive.google.com/open?id=1HMBf4n-hJf_CnNXdS4_HZGm0cjKhbZk0">ContentSuggest Eclipse Plugin</a></li>
+<li><a href="https://drive.google.com/open?id=1d_0Lg9ZGe4LKE7UqQf2SZv5sW5eUE-hS">Plug-in dependencies</a></li>
+</ul>
+
+<p>
+<h3>Related Publications</h3>
+<hr />
+<ul>
+<li class="conference">
+<strong>M. Masudur Rahman</strong> and C.K. Roy, "<i><a href="papers/masud-CASCON2015.pdf">Recommending Relevant Sections from a Webpage about Programming Errors and Exceptions</a></i>", In Proceeding of The 25th Center for Advanced Studies CONference (<b class="premier">CASCON</b> 2015), pp. 81-90, Toronto, Canada, November 2015 (Acceptance rate: 29.57%)
+</li>
+</ul>
+<div class="rounded2">
+<pre>
+@inproceedings{cascon2015masud, 
+author = {Rahman, Mohammad Masudur and Roy, Chanchal K.}, 
+title = {Recommending Relevant Sections from a Webpage About Programming Errors and Exceptions}, 
+booktitle = {Proc. CASCON}, 
+year = {2015}, 
+pages = {181--190} } 
+</pre>
+</div>
+</p>
+</div>
+</td>
+</tr>
+
+<tr>
+<td colspan="3">
+<h4 style="text-align:center">
+<a style="text-decoration:none; cursor:pointer" href="../tools">&larr; Check out other tools by Masud Rahman</a>
+</h4>
+</td>
+</tr>
+
+<tr>
+<td colspan="3">
+<hr />
+<p>&copy; Masud Rahman, Computer Science, University of Saskatchewan, Canada. </p>
+</td>
+</tr>
+</table>
+
+</body>
+</html>
